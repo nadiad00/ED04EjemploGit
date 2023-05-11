5cea94226b8c3e06e0418b61810241f390e19ab7
Nadia <ndalessio@birt.eus>
2023-05-11 16:51:08 +0200

    Versión 1. Commit con cambios básicos

--- src/main/java/com/mycompany/ud04ejemplogit/UD04EjemploGit.java
+++ src/main/java/com/mycompany/ud04ejemplogit/UD04EjemploGit.java
@@ -1,16 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.ud04ejemplogit;
 
 /**
- *
+ * 
  * @author nadia
  */
 public class UD04EjemploGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Ejemplo de Git");
     }
 }
